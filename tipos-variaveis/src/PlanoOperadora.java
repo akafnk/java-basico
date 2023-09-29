5679d8cae8505cfebff8692b91413d95bce67f3c
Frank Gárate <frank.garaterios@gmail.com>
2023-09-28 21:02:34 -0300

    Condicionais

---
+++ tipos-variaveis/src/PlanoOperadora.java
@@ -0,0 +1,33 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "T"; // M / T
+
+        //if (plano == "B") {
+        //    System.out.println("100 minutos de ligação");
+        //} else if (plano == "M") {
+        //    System.out.println("100 minutos de ligação");
+        //    System.out.println("WhatsApp e Instagram grátis");
+        //} else if (plano == "T") {
+        //    System.out.println("100 minutos de ligação");
+        //    System.out.println("WhatsApp e Instagram grátis");
+        //    System.out.println("5Gb Youtube");
+        //}
+
+        switch (plano) {
+            case "B": {
+                System.out.println("100 minutos de ligação");
+                break;
+            }
+            case "M": {
+                System.out.println("Whats e Insta grátis");
+                break;
+            }
+            case "T": {
+                System.out.println("Whats e Insta e 5G grátis");
+                break;
+            }
+
+        }
+
+    }
+}
